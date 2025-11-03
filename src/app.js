f597a98ae44ed789582c62288e31a42c5a8c90f5
Noa <n.barrial34350@gmail.com>
2025-11-03 15:55:55 +0100

    Création fonciton getVolume

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ class Sphere {
         return ((4 / 3) * Math.PI * Math.pow(this.rayon, 3));
     }
 
+    getVolume() {
+
+        return (4 * Math.PI * Math.pow(this.rayon, 2));
+    }
+
 }
 
 module.exports = {
