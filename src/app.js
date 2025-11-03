a7076314adcce992d995b2ecedb7bac4c43486e9
Noa <n.barrial34350@gmail.com>
2025-11-03 16:06:35 +0100

    Création deucieme test (getsurfaceAire) + correction classe

--- src/app.js
+++ src/app.js
@@ -1,26 +1,29 @@
 class Sphere {
 
     constructor(rayon) {
+
         this.rayon = rayon;
     }
 
     getRayon() {
-
+        
         return this.rayon;
     }
 
+
     getSurfaceAire() {
 
-        return ((4 / 3) * Math.PI * Math.pow(this.rayon, 3));
+        return 4 * Math.PI * Math.pow(this.rayon, 2);
     }
+
 
     getVolume() {
 
-        return (4 * Math.PI * Math.pow(this.rayon, 2));
+        return (4 / 3) * Math.PI * Math.pow(this.rayon, 3);
     }
 
 }
 
 module.exports = {
-    Sphere:Sphere
+    Sphere: Sphere
 }
--- test/app.test.js
+++ test/app.test.js
@@ -4,13 +4,13 @@ const expect = require('chai').expect;
 describe('Test fonction de la sphere', function() {
     it('1. Le rayon du sphere', function(done) {
         let s1 = new Sphere(5);
-        expect(s1.getSideLength()).to.equal(5);
+        expect(s1.getRayon()).to.equal(5);
         done();
     });
     
-    it('2. The surface area of the Cube', function(done) {
-        let c2 = new Cube(5);
-        expect(c2.getSurfaceArea()).to.equal(150);
+    it('2. L air de la surface de la sphere', function(done) {
+        let s2 = new Sphere(5);
+        expect(s2.getSurfaceAire()).to.equal(150);
         done();
     });
     
